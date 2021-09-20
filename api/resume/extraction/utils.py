df4aab08076f487c47738f2c7e1713962bb7822e
AK9175 <mailmenehakulkarni@gmail.com>
2021-09-20 11:15:27 +0530

    refactored links

--- api/resume/extraction/utils.py
+++ api/resume/extraction/utils.py
@@ -1,6 +1,38 @@
 import os
 
 
+class Links:
+    def __init__(self):
+        self.urls = {
+            "linkedin": str(),
+            "leetcode": str(),
+            "codechef": str(),
+            "codeforces": str(),
+            "github": list(),
+            "others": list(),
+        }
+
+
+def getLinks(links: dict):
+    tofind = ["github", "linkedin", "leetcode", "codechef", "codeforces"]
+    link = Links()
+    linkscopy = links['url'].copy()
+    for string in tofind:
+        if type(link.urls[string]) == list:
+            for i in links['url']:
+                if string in i:
+                    link.urls[string].append(i)
+                    linkscopy.remove(i)
+        else:
+            for i in links['url']:
+                if string in i:
+                    link.urls[string] = i
+                    linkscopy.remove(i)
+
+    link.urls["others"] = linkscopy
+    return link.urls
+
+
 def getProjects():
     return "Projects TBC"
 
@@ -11,32 +43,3 @@ def getAchievements():
 
 def getHobbies():
     return "Hobbies TBC"
-
-
-def getLinks(links):
-    urls = {
-        "linkedin": None,
-        "github": None,
-        "leetcode": None,
-        "codechef": None,
-        "codeforces": None,
-        "others": None,
-    }
-    github = list()
-    others = list()
-    for i in links["url"]:
-        if "github" in i:
-            github.append(i)
-        elif "linkedin" in i:
-            urls["linkedin"] = i
-        elif "leetcode" in i:
-            urls["leetcode"] = i
-        elif "codechef" in i:
-            urls["codechef"] = i
-        elif "codeforces" in i:
-            urls["codeforces"] = i
-        else:
-            others.append(i)
-    urls["github"] = github
-    urls["others"] = others
-    return urls
